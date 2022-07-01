ce7e7bed08fc57fd5c01412dd070efc0b4fc214a
ltelisabeth <18218032@std.stei.itb.ac.id>
2022-07-01 08:26:31 +0700

    initialfile

---
+++ spring-boot-hello-world/src/main/java/com/codacum/springboothelloworld/controller.java
@@ -0,0 +1,12 @@
+package com.codacum.springboothelloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RESTController;
+
+@RESTController
+public class HelloWorldController {
+    @GetMapping(“/hello-world”)
+        public String HelloWorld(){
+        return “Hello World”;
+    }
+}
